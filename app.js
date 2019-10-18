29d6226f961bbf997e6e2016ebfcc7cbae89a7e6
Luciano Quiroga <nanoquiroga180@gmail.com>
2019-10-18 03:15:13 -0300

    usuario y token listo

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
 
+// BodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
 // Conexión a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/aplicacionDB', (err, res) => {
+mongoose.connection.openUri('mongodb://localhost:27017/aplicacionDB', { useCreateIndex: true }, (err, res) => {
     if (err) throw err;
 
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
@@ -16,14 +26,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/aplicacionDB', (err, res)
 
 
 // Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
