9996cd2a34894864dbec87e00d7ff189ae5f1ec4
Luciano Quiroga <nanoquiroga180@gmail.com>
2019-10-18 10:13:34 -0300

    Arreglos finales

--- config/config.js
+++ config/config.js
@@ -4,3 +4,5 @@ module.exports.SEED = '@este-es@-un-seed-dificil';
 
 // Google
 module.exports.CLIENT_ID = '373734158143-aa90cge0tj91pa82j45hdvl196kv349d.apps.googleusercontent.com';
+
+module.exports.GOOGLE_SECRET = 'kjWILj7SA8SgyW1gKFrSrPZL';
